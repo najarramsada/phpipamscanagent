2d1a1f3e4e147750388c66d0e25eab498690e6ef
Najar Ramsada <40881620+najarramsada@users.noreply.github.com>
2018-09-21 11:45:19 +1000

    updated cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --network=host -v /src/config.php:/ipamscan/config.php imanage/phpipamscan-alpine:1.0
+docker run --rm --network=host -v /src/config.php:/ipamscan/config.php najarramsada/phpipamscanagent
